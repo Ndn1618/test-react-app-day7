7adf9ff33f363ecec7934f60c1ae6d3af45fed99
Ndn1618 <ndilbar1618@gmail.com>
2020-12-17 11:09:19 +0500

    Disable button when password's length is less than 6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+  const [password, setPassword] = useState('')
+  const [disabled, setDisabled] = useState(false)
+
+  const onSubmit = (evt) => {
+    evt.preventDefault()
+    console.log('Submitted', password)
+  }
+
+  useEffect(() => {
+    setDisabled(password.length < 6)
+  }, [password])
+
   return (
     <>
-      App
+      <form onSubmit={onSubmit}>
+        <label htmlFor='password-input'>
+          Enter password
+          <input onChange={evt => setPassword(evt.target.value)} type='password' id='password-input' placeholder='Password'></input>
+        </label>
+        <button type='submit' disabled={disabled}>Login</button>
+      </form>
     </>
   );
 }
